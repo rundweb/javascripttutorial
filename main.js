41e8ca65e8135aa2f2fe6ade07fbe908846337ad
Rifaldo <pn4066283@gmail.com>
2023-02-13 14:28:09 +0700

    tutorial bagian satu

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-// menambahkan array
-let namaGuru = ["guru satu", "guru dua", "guru tiga"];
+// let nama = prompt("hallo selamat datang silahkan masukan nama kamu");
+// let usia = prompt("usia kamu berapa");
+let x =prompt("masukan angka kamu disini");
+let y =prompt("ditambah dengan angka kamu disni");
+let a =prompt(`tebakan kamu berapa ${x} + ${y} berapa ?`)
+let z =parseFloat(x) +parseFloat(y);
+let b;
 
-//menambahkan data array
-namaGuru.push("guru empat", "guru tiga");
+if (a == z) {
+  b = "benar";
+} else {
+  b = "salah";
+}
 
-// menghapsu array kesatu
-namaGuru.shift();
-
-// menghapus array terakhir
-namaGuru.pop();
-alert(namaGuru);
+// alert(`hallo ${nama} usia kamu ${usia}`);
+alert(`jawaban kamu ${a} /dan jawaban yang benar adalah ${z} jadi jawaban kamu ${b}`);

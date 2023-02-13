4f6689535ae6061d5e3bda1cfaefa90808812f2d
Rifaldo <pn4066283@gmail.com>
2023-02-13 13:10:38 +0700

    pengkondisian

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
-let usia = prompt("masukan usia kamu");
-alert("usia kamu adalah : " + usia );
+let nama = "rundweb";
+let usia = 23;
+let tinggiBadan = 175.6;
+let beratBadan;
+let pacar = 2;
+
+beratBadan = 81;
+
+//pengkodisian menggunakan if
+// if (pacar == null) {
+//   pacar = "belum punya pacar";
+// } else {
+//   pacar = "sudah punya pacar";
+// }
+
+
+
+// pengkondisian menggunakan switch
+switch (pacar) {
+    case 1:
+        pacar = "saya mempunyai pacar"
+        break;
+
+    default:
+        pacar = "saya tidak mempunyai pacar"
+        break;
+}
+
+alert(
+  `nama saya ${nama}, usia saya ${usia}, tinggi badan saya adalah ${tinggiBadan}, dan berat badan saya ${beratBadan}, dan pacar saya ${pacar}`
+);

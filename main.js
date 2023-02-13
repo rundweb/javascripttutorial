6b08b815b44b4eea4b45504ccbeffadbb446646e
Rifaldo <pn4066283@gmail.com>
2023-02-13 13:22:30 +0700

    oprator

--- main.js
+++ main.js
@@ -1,31 +1,12 @@
-let nama = "rundweb";
-let usia = 23;
-let tinggiBadan = 175.6;
-let beratBadan;
-let pacar = 2;
+// menambahkan array
+let namaGuru = ["guru satu", "guru dua", "guru tiga"];
 
-beratBadan = 81;
+//menambahkan data array
+namaGuru.push("guru empat", "guru tiga");
 
-//pengkodisian menggunakan if
-// if (pacar == null) {
-//   pacar = "belum punya pacar";
-// } else {
-//   pacar = "sudah punya pacar";
-// }
+// menghapsu array kesatu
+namaGuru.shift();
 
-
-
-// pengkondisian menggunakan switch
-switch (pacar) {
-    case 1:
-        pacar = "saya mempunyai pacar"
-        break;
-
-    default:
-        pacar = "saya tidak mempunyai pacar"
-        break;
-}
-
-alert(
-  `nama saya ${nama}, usia saya ${usia}, tinggi badan saya adalah ${tinggiBadan}, dan berat badan saya ${beratBadan}, dan pacar saya ${pacar}`
-);
+// menghapus array terakhir
+namaGuru.pop();
+alert(namaGuru);
